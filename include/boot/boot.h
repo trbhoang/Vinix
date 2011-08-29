c7fe2353353a4261319bb3c53cb33480f1c87d96
Hoang Tran <trbhoang@gmail.com>
2011-08-29 16:38:00 +0700

    goto Protected Mode

---
+++ include/boot/boot.h
@@ -0,0 +1,13 @@
+/**
+ * @file   boot.h
+ * @brief  bootloader values
+ * @author Hoang Tran <trbhoang@gmail.com>
+ */
+
+#ifndef BOOT_H
+#define BOOT_H
+
+
+#define STAGE2_SEC    0x50  /* bootloader stage2 segment */
+
+#endif
