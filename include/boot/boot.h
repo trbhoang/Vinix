338f6cdcc3d31949acef059a1e1569239ef6b155
Hoang Tran <trbhoang@gmail.com>
2011-09-03 15:45:24 +0700

    make simple bootloader that does only GDT loading

--- include/boot/boot.h
+++ include/boot/boot.h
@@ -8,6 +8,12 @@
 #define BOOT_H
 
 
-#define KERN_SEC    0x1000
+#define STAGE2_SEC      0X50
+#define STAGE2_BASE     0X500
+#define KERN_SEC        0x1000
+#define KERN_BASE       0X10000
+#define CODE_SEC_DESC   0X8
+#define DATA_SEC_DESC   0X10
+
 
 #endif
