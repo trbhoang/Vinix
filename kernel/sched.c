a68dbe342e70d5ef0f69f60b12a9214b1431d6f6
Hoang Tran <trbhoang@gmail.com>
2011-09-29 23:38:34 +0700

    tty_init

--- kernel/sched.c
+++ kernel/sched.c
@@ -1,6 +1,26 @@
+/*
+ * 'sched.c' is the main kernel file. It contains scheduling primitives
+ * (sleep_on, wakeup, schedule etc) as well as a number of simple system
+ * call functions (type getpid(), which just extracts a field from current-task).
+ */
+#include <linux/sched.h>
+
+
+
 #define PAGE_SIZE 4096
 
 long startup_time = 0;
+
+union task_union {
+  struct task_struct task;
+  char stack[PAGE_SIZE];
+};
+
+static union task_union init_task = {INIT_TASK,};
+
+
+struct task_struct *task[NR_TASKS] = {&(init_task.task), };
+
 
 /* allocate a buffer for stack */
 long user_stack[PAGE_SIZE>>2];  /* (4096 >> 2) * 4 =  */
@@ -9,3 +29,13 @@ struct {
   long * a;
   short  b;                     /* 2 bytes */
 } stack_start = {&user_stack[PAGE_SIZE>>2], 0x10}; /* will become (ESP, SS selector) */
+
+
+
+void wake_up(struct task_struct **p)
+{
+  if (p && *p) {
+    (**p).state = 0;
+    *p = NULL;
+  }
+}
--- kernel/tty_io.c
+++ kernel/tty_io.c
@@ -98,4 +98,89 @@ void tty_init(void)
   con_init();
 }
 
+void tty_intr(struct tty_struct *tty, int signal)
+{
+  int i;
 
+  if (tty->pgrp <= 0) return;
+  for (i = 0; i < NR_TASKS; i++)
+    if (task[i] && task[i]->pgrp == tty->pgrp)
+      task[i]->signal |= 1 << (signal - 1);
+}
+
+void copy_to_cooked(struct tty_struct *tty)
+{
+  signed char c;
+
+  while (!EMPTY(tty->read_q) && !FULL(tty->secondary)) {
+    GETCH(tty->read_q, c);
+    if (c == 13)
+      if (I_CRNL(tty))
+        c = 10;
+      else if (I_NOCR(tty))
+        continue;
+      else ;
+    else if (c == 10 && I_NLCR(tty))
+      c = 13;
+
+    if (I_UCLC(tty)) c = tolower(c);
+
+    if (L_CANON(tty)) {
+      if (c == ERASE_CHAR(tty)) {
+        if (EMPTY(tty->secondary) || (c = LAST(tty->secondary)) == 10 || c == EOF_CHAR(tty)) continue;
+        if (L_ECHO(tty)) {
+          if (c < 32) PUTCH(127, tty->write_q);
+          PUTCH(127, tty->write_q);
+          tty->write(tty);
+        }
+        DEC(tty->secondary.head);
+        continue;
+      }
+      if (c == STOP_CHAR(tty)) {
+        tty->stopped = 1;
+        continue;
+      }
+      if (c == START_CHAR(tty)) {
+        tty->stopped = 0;
+        continue;
+      }
+    }
+    if (!L_ISIG(tty)) {
+      if (c == INTR_CHAR(tty)) {
+        tty_intr(tty, SIGINT);
+        continue;
+      }
+    }
+    if (c == 10 || c == EOF_CHAR(tty))
+      tty->secondary.data++;
+    if (L_ECHO(tty)) {
+      if (c == 10) {
+        PUTCH(10, tty->write_q);
+        PUTCH(13, tty->write_q);
+      } else if (c < 32) {
+        if (L_ECHOCTL(tty)) {
+          PUTCH('^', tty->write_q);
+          PUTCH(c + 64, tty->write_q);
+        }
+      } else PUTCH(c, tty->write_q);
+      tty->write(tty);
+    }
+    PUTCH(c, tty->secondary);
+  }
+  wake_up(&tty->secondary.proc_list);
+}
+
+/*
+ * Jeh, sometimes I really like the 386.
+ * This routine is called from an interrupt,
+ * and there should be absolutely no problem
+ * with sleeping even in an interrupt (I hope).
+ * Of course, if somebody proves me wrong, I'll
+ * hate intel for all time :-). We'll have to
+ * be careful and see to reinstating the interrupt
+ * chips before calling this, though.
+ */
+void do_tty_interrupt(int tty)
+{
+  copy_to_cooked(tty_table + tty);
+}
